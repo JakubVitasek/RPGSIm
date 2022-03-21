8abbedcb807e5a10ebc9e2b3bbd81ce3e87d1ef3
Jakub Vitásek <jakub.vitasek@educhem.cz>
2022-03-21 15:03:33 +0100

    jsut lepší verze stejného programu

--- RPGSIm/Program.cs
+++ RPGSIm/Program.cs
@@ -14,45 +14,53 @@ int Spanek = 0;
 
 int Level = 0;
 
+int cas = 10000;
+
+int cas2 = cas / 1000;
+
 while(true)
 {
     Console.WriteLine($"{jmenoPostavy} je unavený na {Spanek}%");
     Console.WriteLine($"Chce se {jmenoPostavy}  vyspat nebo vyrazit na Quest? Q/V");
-    string input = Console.ReadLine();
+    string input = Console.ReadLine().ToLower();
     Random ran = new Random();
     int cislo = ran.Next(0, 3);
     Console.Clear();
 
-    if (input == "Q")
+    if (input == "q")
     {
         string vygAtr = poleAtr[cislo];
-        Console.WriteLine($"{jmenoPostavy} se vydává se  quest {vygAtr}, vratí se za 10 sec");
-        Console.WriteLine(". . .");
-        Thread.Sleep(1000);
+        Console.WriteLine($"{jmenoPostavy} se vydává se  quest {vygAtr}, vratí se za {cas2} sec");
+        Console.WriteLine(". . . . . . . . .");
+        Thread.Sleep(cas);
         Console.WriteLine($"{jmenoPostavy} se z výpravy vrátil živ a zdráv");
-        Console.WriteLine(". . .");
+        Console.WriteLine(". . . . . . . . .");
         Spanek = Spanek + 10;
         atributy[vygAtr]  +=10;
         Level = Level + 1;
+        cas = cas + 1000;
         Console.WriteLine($"{jmenoPostavy} má Level {Level}");
+        Console.WriteLine(". . . . . . . . .");
         foreach (var radekSlovniku in atributy)
         {
             Console.WriteLine($"Skill : {radekSlovniku.Key}, Level : {radekSlovniku.Value}");
         }
-        Console.WriteLine(". . .");
+        Console.WriteLine(". . . . . . . . .");
     }
 
-    if(input == "V")
+    if(input == "v")
     {
-        Console.WriteLine($"{jmenoPostavy} spinká");
-        Console.WriteLine(". . .");
+        Console.WriteLine($"{jmenoPostavy} spinká <3");
+        Console.WriteLine(". . . . . . . . .");
         Thread.Sleep(10000);
         Spanek = 0;
+        Console.WriteLine($"{jmenoPostavy} je už zase plný energie");
+        Console.WriteLine(". . . . . . . . .");
         foreach (var radekSlovniku in atributy)
         {
             Console.WriteLine($"Skill : {radekSlovniku.Key}, Level : {radekSlovniku.Value}");
         }
-        Console.WriteLine(". . .");
+        Console.WriteLine(". . . . . . . . .");
     }
      
 }
